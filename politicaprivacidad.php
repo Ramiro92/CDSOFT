b89c26c62e9bde73d00094124aaaee0671f42468
Ramiro Amaya <ramiro_amaya16@hotmail.com>
2023-11-17 18:22:51 -0600

    Paginas iniciales

---
+++ politicaprivacidad.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>CDSOFT</title>
+	<link t rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" ></script>
+</head>
+<body>
+	<header>
+		<nav class="navbar navbar-expand-lg" style="background: #003a70;">
+			<div class="container-fluid">
+				<a class="navbar-brand"><img src="/prueba/img/Logo.webp" alt="" width="250"></a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+						<li class="nav-item">
+							<a class="nav-link active" aria-current="page" href="team.php" style="color:white; font-weight: bold;">CDSOFT</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#" style="color:white; font-weight: bold;">Privacy Policity</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<div class=" py-5 bg-light">
+			<center>
+				<h1 style="color: black;">Privacy Policity</h1></center>
+			</div>
+
+			<div class="container">
+				<div class="row">
+
+					<div class="py-5">
+						<div class="card border-primary  text-info-emphasis bg-info-subtle mb-3" style="max-width: auto;">
+							<!-- <div class="card-header">Header</div> -->
+							<div class="card-body">
+								<!-- <h5 class="card-title">Primary card title</h5> -->
+								<p class="card-text">Your privacy is important to us. It is <strong>Universidad Francisco Gavidia's</strong> policy to respect your privacy regarding any information we may collect from you through our apps.
+We only ask for personal information when we truly need it to provide a service to you.</p>
+<p class="card-text">We collect it by fair and lawful means, with your knowledge and consent.</p>
+<p class="card-text">We also let you know why we’re collecting it and how it will be used.</p>
+<p class="card-text">We only retain collected information for as long as necessary to provide you with your requested service.</p>
+<p class="card-text">What data we store, we’ll protect within commercially acceptable means to prevent loss and theft, as well as unauthorized access, disclosure, copying, use or modification.</p>
+<p class="card-text">We don’t share any personally identifying information publicly or with third-parties, except when required to by law.</p>
+<p class="card-text">Our apps may link to external sites that are not operated by us. Please be aware that we have no control over the content and practices of these sites, and cannot accept responsibility or liability for their respective privacy policies.</p>
+<p class="card-text">You are free to refuse our request for your personal information, with the understanding that we may be unable to provide you with some of your desired services. Your continued use of our app will be regarded as acceptance of our practices around privacy and personal information. If you have any questions about how we handle user data and personal information, feel free to contact us.</p>
+<p class="card-text">This policy is effective as of January 2, 2020.</p>
+							</div>
+						</div>
+					</div>
+				</header>
+
+				<footer class="py-5 bg-dark-subtle">
+					<div class="container" >
+						<div class="row">
+							<div class="col-6 col-md-6">
+								<p style="font-weight:bold"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pc-display" viewBox="0 0 16 16">
+									<path d="M8 1a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1V1Zm1 13.5a.5.5 0 1 0 1 0 .5.5 0 0 0-1 0Zm2 0a.5.5 0 1 0 1 0 .5.5 0 0 0-1 0ZM9.5 1a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5ZM9 3.5a.5.5 0 0 0 .5.5h5a.5.5 0 0 0 0-1h-5a.5.5 0 0 0-.5.5ZM1.5 2A1.5 1.5 0 0 0 0 3.5v7A1.5 1.5 0 0 0 1.5 12H6v2h-.5a.5.5 0 0 0 0 1H7v-4H1.5a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .5-.5H7V2H1.5Z"/>
+								</svg> Centro de Desarrollo de Software</p>
+							</div>
+							<div class="col-6 col-md-6">
+								<p>Developed By CDSOFT - © 2023</p>
+								<p>Powered By <a href="https://www.youtube.com/watch?v=vKFSi9QeVH8" style="text-decoration: none;">UFG Cloud</a></p>
+							</div>
+						</div>
+					</div>
+				</footer>
+			</body>
+			</html>
